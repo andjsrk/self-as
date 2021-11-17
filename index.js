161a41d3d9442dbb75d3d5aa432a732992f56c81
andjsrk <43586090+andjsrk@users.noreply.github.com>
2021-11-18 04:48:45 +0900

    Update usage

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-module.exports = function selfAs(callback) {
-	if (typeof callback !== 'function') {
-		throw new TypeError('callback is not a function')
+module.exports = function selfAs(value) {
+	return function (callback) {
+		if (typeof callback !== 'function') {
+			throw new TypeError('callback is not a function')
+		}
+		return callback(value)
 	}
-	return callback
 }
